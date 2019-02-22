3f9069443d86b04d68722c8d92c8a1aa18e70872
Animikh Aich <animikhaich@gmail.com>
2019-02-23 04:43:51 +0530

    Added the YOLOv3 script. Further changes to be made; yet to be streamlined

--- temp.py
+++ temp.py
@@ -6,50 +6,10 @@ Created on Wed Nov 14 16:28:45 2018
 
 @author: AkhileshAkku
 """
-
+import warnings
+warnings.simplefilter('ignore')
 from imageai.Detection import ObjectDetection
 import os
-
-execution_path = os.getcwd()
-
-
-detector = ObjectDetection()
-detector.setModelTypeAsYOLOv3()
-detector.setModelPath( os.path.join(execution_path , "yolo.h5"))
-detector.loadModel()
-
-
-custom_objects = detector.CustomObjects(person=True, motorcycle=True)
-detections = detector.detectCustomObjectsFromImage(custom_objects=custom_objects, input_image=os.path.join(execution_path , "frame982.jpg"), output_image_path=os.path.join(execution_path , "frame982yolooutput.jpg"), minimum_percentage_probability=30)
-
-print(detections)
-# =============================================================================
-# for eachObject in detections:
-#     print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
-#     print("--------------------------------")
-# 
-# =============================================================================
-#OBJECT DETECTION FROM CUSTOM VIDEO
-
-# =============================================================================
-# from imageai.Detection import VideoObjectDetection
-# import os
-# 
-# execution_path = os.getcwd()
-# 
-# detector = VideoObjectDetection()
-# detector.setModelTypeAsRetinaNet()
-# detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
-# detector.loadModel()
-# 
-# custom_objects = detector.CustomObjects(person=True, motorcycle=True)
-# 
-# video_path = detector.detectCustomObjectsFromVideo(custom_objects=custom_objects, input_file_path=os.path.join(execution_path, "test_video2.mp4"),
-#                                 output_file_path=os.path.join(execution_path, "traffic_custom_detected")
-#                                 , frames_per_second=15, log_progress=True)
-# print(video_path)
-# ==========================================================================
-    
 import math
 
 import matplotlib.pyplot as plt
@@ -58,11 +18,27 @@ from PIL import Image
 import numpy as np
 import cv2
 
-frame = cv2.imread("frame982yolooutput.jpg")
+execution_path = "C:/Users/animi/Desktop/Object Detection Using ImageAI/"
+
+detector = ObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath(execution_path+"yolo.h5")
+detector.loadModel()
+
+
+custom_objects = detector.CustomObjects(person=True, motorcycle=True)
+detections = detector.detectCustomObjectsFromImage(custom_objects=custom_objects, input_image="frame982.jpg", output_image_path="frame982yolooutput.jpg", minimum_percentage_probability=30)
+
+print(detections)
+
+
+
+frame = cv2.imread("frame982.jpg")
 detected_copy = frame.copy()
 detected_copy = cv2.cvtColor(detected_copy, cv2.COLOR_BGR2RGB)
 
 head_detected = detected_copy.copy()
+
 
 def intersection(a,b):
   x1 = max(a[0], b[0])
@@ -76,7 +52,7 @@ list_of_heads = []
 for eachObject in detections:
     if eachObject['name'] == 'motorcycle':
         for secondObject in detections:
-            if secondObject['name'] == 'person' and  abs(eachObject['box_points'][0]-secondObject['box_points'][0]) < 50:
+            if secondObject['name'] == 'person' and abs(eachObject['box_points'][0]-secondObject['box_points'][0]) < 50:
                 intersection_rect = intersection(eachObject['box_points'], secondObject['box_points'])
                 intersection_top_left = (intersection_rect[0], intersection_rect[1])
                 intersection_bottom_right = (intersection_rect[2], intersection_rect[3])
@@ -94,14 +70,14 @@ for eachObject in detections:
                 person_left_top = (secondObject['box_points'][0], secondObject['box_points'][1])
                 person_right_bottom = (secondObject['box_points'][2], secondObject['box_points'][3] - 3 * person_height //4)
 # =============================================================================
-#                 if area>0: 
+#                 if area>0:
 #                     cv2.rectangle(detected_copy, intersection_top_left, intersection_bottom_right, (0,255,0),  2)
 # =============================================================================
                 
                 if area > 0 and area > 8000 :
                     list_of_rects.append((bigbox_left, bigbox_top, bigbox_right, bigbox_bottom))
                     cv2.rectangle(detected_copy, (bigbox_left, bigbox_top), (bigbox_right, bigbox_bottom), (0,255,0),  2)
-# cv2.line(detected_copy, top_left, bottom_right, (0, 255, 0), 2, lineType = 8)
+                    # cv2.line(detected_copy, top_left, bottom_right, (0, 255, 0), 2, lineType = 8)
                     cv2.imwrite("detected.jpg", detected_copy)
                     
                     
@@ -114,35 +90,14 @@ for eachObject in detections:
 print(list_of_heads)
 cv2.imshow("detected", detected_copy)
 
-im = cv2.imread("head_detected.jpg")    
+cv2.waitKey(0)
+
+im = cv2.imread("frame982.jpg")
 j = 1
 for i in list_of_heads:
     cropped_img = im[i[1]:i[3], i[0]:i[2]]
     cv2.imwrite('croppedhead'+str(j)+'.jpg', cropped_img)
     j += 1
-    
-#cv2.imshow('cropped', cropped_img)
-    
 
+cv2.destroyAllWindows()
 
-
-# =============================================================================
-# im = np.array(Image.open('frame246yolooutput.jpg'), dtype=np.uint8)
-# 
-# # Create figure and axes
-# fig,ax = plt.subplots(1)
-# 
-# # Display the image
-# ax.imshow(im)
-# 
-# # Create a Rectangle patch
-# rect = patches.Rectangle((intersection_rect[0], intersection_rect[1]), intersection_rect[2], intersection_rect[3],linewidth=2,edgecolor='g', facecolor = 'None')
-# 
-# # Add the patch to the Axes
-# ax.add_patch(rect)
-# 
-# plt.show()    
-# =============================================================================
-
-    
-    
