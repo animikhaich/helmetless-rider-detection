2ed414a18407503d8cda28e9d608fa25070dc8ce
Animikh Aich <animikhaich@gmail.com>
2020-02-03 23:35:11 +0530

    Added Random Non-Overlapping Coordinate Generator

--- synthetic_data_generation/OD_datagen.py
+++ synthetic_data_generation/OD_datagen.py
@@ -4,8 +4,8 @@ from PIL import Image, ImageDraw
 import numpy as np
 import lxml.etree as etree
 
-
 class DataGenerator:
+    
     def __init__(
         self,
         bg_root_path='backgrounds',
@@ -33,10 +33,44 @@ class DataGenerator:
         self.object_to_bg_width_ratio = object_to_bg_width_ratio
         self.min_objects = min_objects
         self.max_objects = max_objects
+        self.existing_coords = []
 
         # Verify and create the output directories
         if not os.path.isdir(self.output_image_path): os.makedirs(self.output_image_path)
         if not os.path.isdir(self.output_annot_path): os.makedirs(self.output_annot_path)
+
+    def generate_coordinates(self, bg_w, bg_h, object_w, object_h):
+        for _ in range(100):
+            # Randomly generated X and Y coordinates
+            x_coord = np.random.randint(0, bg_w-object_w)
+            y_coord = np.random.randint(0, bg_h-object_h)
+            
+            # Extract the coordinates of the image
+            x1, y1, x2, y2 = x_coord, y_coord, x_coord + object_w, y_coord + object_h
+            
+            # Initialize Overlaps, Because default value is necessary
+            overlaps = [False]
+
+            # Check if any of the existing coordinates overlap with the randomly created one
+            for x3, y3, x4, y4 in self.existing_coords:
+                diff_width = min(x2, x4) - max(x1, x3)
+                diff_height = min(y2, y4) - max(y1, y3)
+
+                intersection_area = diff_width * diff_height
+
+                # Check if there is overlap by checking if the intersection area is negative
+                if diff_width < 0 and diff_height < 0 or intersection_area < 0:
+                    overlaps.append(False)
+                else:
+                    overlaps.append(True)
+            
+            # if there are no overlaps with any existing coordinates, return the new coordinates
+            if not any(overlaps):
+                self.existing_coords.append([x1, y1, x2, y2])
+                return True, (x1, y1, x2, y2)
+
+        # If No viable coordinates are found, return False
+        return False, (0, 0, 0, 0)
 
     def main(self, total_combinations=200):
         for combination in tqdm.tqdm(range(total_combinations)):
@@ -58,21 +92,27 @@ class DataGenerator:
             xml_width.text = str(bg_w)
             xml_height.text = str(bg_h)
             xml_depth.text = str(3)
+            
+            # Reset the existing coordinates for each new image
+            self.existing_coords = []
 
             for object_path in object_paths:
-                # Read the object and convert from BGR to RGB color space
+                # Read the object without any modifications to it's number of channels
                 object_image = cv2.imread(object_path, cv2.IMREAD_UNCHANGED)
+                
+                # Skip the object if it is in grayscale
                 try:
                     object_h, object_w, channels = object_image.shape
                 except:
                     continue
                 
+                # Convert the image to RGB by conserving the number of channels
                 if channels == 4:
                     object_image = cv2.cvtColor(object_image, cv2.COLOR_BGRA2RGBA)
                 else:
                     object_image = cv2.cvtColor(object_image, cv2.COLOR_BGR2RGB)
 
-                # Scale the object as per the background
+                # Scale the object as per the background based on width or height conserving the original ratio
                 if object_h < object_w:
                     target_height = round(bg_h * self.object_to_bg_width_ratio)
                     object_image = imutils.resize(object_image, height=target_height)
@@ -87,18 +127,20 @@ class DataGenerator:
                 else:
                     object_image = Image.fromarray(object_image)
 
-                # Generate the coordinates to paste the image
-                x_coord = np.random.randint(0, bg_w-object_w)
-                y_coord = np.random.randint(0, bg_h-object_h)
+                # Generate Random Coordinates
+                generated, coordinates = self.generate_coordinates(bg_w, bg_h, object_w, object_h)
+                
+                # if non overlapping coordinates aren't generated in 100 tries, continue to the next object
+                if not generated: continue
 
+                # Unpack the coordinates
+                x1, y1, x2, y2 = coordinates
+
+                # If the object is RGBA, paste as a mask on the background
                 if channels == 4:
-                    bg_image.paste(object_image, (x_coord, y_coord), mask=object_image)
+                    bg_image.paste(object_image, (x1, y1), mask=object_image)
                 else:
-                    bg_image.paste(object_image, (x_coord, y_coord))
-                
-                
-                # Extract the coordinates of the image
-                x1, y1, x2, y2 = x_coord, y_coord, x_coord + object_w, y_coord + object_h
+                    bg_image.paste(object_image, (x1, y1))
 
                 # Write XML Elements
                 xml_object = SubElement(top, 'object')
